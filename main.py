484224680f26b310ebea03554b633858b33f4b4b
MoThom <43882836+MoThom@users.noreply.github.com>
2018-10-05 16:10:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 16:08:27 2018
+
+@author: Moritz Thom
+"""
+
+
+import modules
+
+
+
+#Example:
+segmentation(nameOfVid='example.mp4', thresh=140, percent=50)
